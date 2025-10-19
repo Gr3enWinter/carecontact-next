1d1b95d8e67641b6eebfd96cd56792cb56fda8cd
Gr3enWinter <addisonlenna@gmail.com>
2025-10-18 23:11:49 -0400

    fix(api): correct relative import to src/lib/supabaseServer from scrape routes

--- app/api/scrape/directory/route.ts
+++ app/api/scrape/directory/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import * as cheerio from 'cheerio'
-import { adminClient } from '../../../src/lib/supabaseServer'
+import { adminClient } from '../../../../src/lib/supabaseServer'
 
 export const runtime = 'nodejs'
 export const maxDuration = 30
